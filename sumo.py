3c8b045ccb642bded3917610427731df541d80a4
the falcons #1097 <thefalcons1097@gmail.com>
2024-09-06 14:44:36 +0300

    Add files via upload

--- sumo.py
+++ sumo.py
@@ -6,7 +6,7 @@ import ev3dev.auto as ev3
 import threading
 import time
 
-## Some helpers ##
+## Functions to translate he ps4 input to motors speed ##
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
@@ -21,18 +21,24 @@ def dc_clamp(value):
 
 ## Initializing ##
 print("Finding ps4 controller...")
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-ps4dev = devices[0].fn
 
+#getting a list of all connected bluetooth devices
+devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+
+# getting the ps4 controller from the list
+ps4dev = devices[0].fn 
 gamepad = evdev.InputDevice(ps4dev)
 
+# setting deafult variables
 forward_speed = 0
 speed_hands = 0
 side_speed = 0
 running = True
 
+# setting a motor listener thread
 class MotorThread(threading.Thread):
     def __init__(self):
+        # defining motors
         self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
         self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
         threading.Thread.__init__(self)
@@ -40,18 +46,21 @@ class MotorThread(threading.Thread):
     def run(self):
         print("Engine running!")
         while running:
+            # running the two motors at the variable speed set by a loop always
             self.right_motor.run_forever(speed_sp=dc_clamp(forward_speed+side_speed))
             self.left_motor.run_forever(speed_sp=dc_clamp(-forward_speed+side_speed))
         self.right_motor.stop()
         self.left_motor.stop()
 
-
+# starting the motor listener
 motor_thread = MotorThread()
 motor_thread.setDaemon(True)
 motor_thread.start()
 
+# setting a motor listener thread
 class HandsThread(threading.Thread):
     def __init__(self):
+        # defining motors
         self.right_motor = ev3.MediumMotor(ev3.OUTPUT_D)
         self.left_motor = ev3.MediumMotor(ev3.OUTPUT_A)
         threading.Thread.__init__(self)
@@ -59,32 +68,34 @@ class HandsThread(threading.Thread):
     def run(self):
         print("Engine running!")
         while running:
+            # running the two motors at the variable speed set by a loop always
             self.right_motor.run_forever(speed_sp=dc_clamp(-speed_hands))
             self.left_motor.run_forever(speed_sp=dc_clamp(-speed_hands))
         self.right_motor.stop()
         self.left_motor.stop()
 
-
+# starting the motor listener
 hands_thread = HandsThread()
 hands_thread.setDaemon(True)
 hands_thread.start()
 
+# checking for events in the ps4 controller using "https://github.com/codeadamca/ev3-python-ps4?tab=readme-ov-file#button-and-stick-events"
 for event in gamepad.read_loop():   #this loops infinitely
-    if event.type == 3:             #A stick is moved
-        if event.code == 0:         #X axis on left stick
+    if event.type == 3:             
+        if event.code == 0:         
             forward_speed = -scale_stick(event.value)
-        if event.code == 1:         #Y axis on left stick
+        if event.code == 1:        
             side_speed = -scale_stick(event.value)
         if side_speed < 100 and side_speed > -100:
             side_speed = 0
         if forward_speed < 100 and forward_speed > -100:
             forward_speed = 0
-        if event.code == 4:         #Y axis on right stick
+        if event.code == 4:         
             speed_hands = -scale_stick(event.value)
         if speed_hands < 100 and speed_hands > -100:
             speed_hands = 0
-    if event.type == 1 and event.code == 305 and event.value == 1:
-        print("X button is pressed. Stopping.")
+    if event.type == 4 and event.code == 305 and event.value == 1:
+        print("O button is pressed. Stopping.")
         running = False
-        time.sleep(0.5) # Wait for the motor thread to finish
+        time.sleep(0.5) 
         break 
